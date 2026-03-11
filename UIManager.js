8f566ec855b17f1c874ded9842b9e99a19f2347a
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2026-03-11 17:55:15 -0400

    fix copy paste contact

---
+++ UIManager.js
@@ -0,0 +1,55 @@
+class UIManager {
+    constructor() {
+        this.menuBtn = document.getElementById("menuBtn");
+        this.dropdown = document.getElementById("tabsDropdown");
+
+        document.addEventListener("DOMContentLoaded", () => { this.init(); });
+    }
+
+    init() {
+        this.menuBtn?.addEventListener("click", (e) => {
+            e.stopPropagation();
+            this.toggleDropdown();
+        })
+
+        document.addEventListener("click", (e) => {
+            if (!this.dropdown.contains(e.target) || !this.menuBtn.contains(e.target)) {
+                this.closeDropdown();
+            }
+        });
+
+        document.querySelectorAll('[data-copy]').forEach(item => {
+            item.addEventListener("click", () => {
+                navigator.clipboard.writeText(item.dataset.copy);
+                const tooltip = item.querySelector('.copy-tooltip');
+                tooltip.textContent = 'Copié!';
+                setTimeout(() => {
+                    tooltip.innerHTML = 'Copier <i class="fa-solid fa-copy copy-icon"></i>';
+                }, 2000);
+            });
+        });
+
+        // function copyPhone() {
+        //     navigator.clipboard.writeText('+14385277030');
+        //     const tooltip = document.getElementById('copyTooltip');
+        //     tooltip.textContent = 'Copié!';
+        //     setTimeout(() => {
+        //         tooltip.innerHTML = 'Copier <i class="fa-solid fa-copy copy-icon"></i>';
+        //     }, 2000);
+        // }
+    }
+
+    openDropdown() {
+
+    }
+
+    closeDropdown() {
+        this.dropdown.classList.add("hidden");
+    }
+
+    toggleDropdown() {
+        this.dropdown?.classList.toggle("hidden");
+    }
+}
+
+const uiManager = new UIManager();
